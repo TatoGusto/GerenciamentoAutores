cd67cd33fde2b83188c0c611aea9f0a25af5ea9e
OtavioAugusto <santostatogusto@gmail.com>
2026-03-21 21:00:26 -0300

    model criado

---
+++ GerenciamentoAutores/src/main/java/com/oatw/GerenciamentoAutores/Models/AutorModel.java
@@ -0,0 +1,54 @@
+package com.oatw.GerenciamentoAutores.Models;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "tb_autor")
+public class AutorModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "CodigoAutor")
+    private Long id;
+    private String nome;
+    @Column(name = "País de origem")
+    private String nacionalidade;
+    private LocalDate dataNascimento;
+
+    public AutorModel() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNacionalidade() {
+        return nacionalidade;
+    }
+
+    public void setNacionalidade(String nacionalidade) {
+        this.nacionalidade = nacionalidade;
+    }
+
+    public LocalDate getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(LocalDate dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+}
