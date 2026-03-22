01a58c7b324e37a347f569da7e31944fc2168c70
OtavioAugusto <santostatogusto@gmail.com>
2026-03-21 21:22:37 -0300

    service criado

---
+++ GerenciamentoAutores/src/main/java/com/oatw/GerenciamentoAutores/Services/AutorService.java
@@ -0,0 +1,31 @@
+package com.oatw.GerenciamentoAutores.Services;
+
+import com.oatw.GerenciamentoAutores.Models.AutorModel;
+import com.oatw.GerenciamentoAutores.Repositories.AutorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class AutorService {
+
+    @Autowired
+    AutorRepository autorRepository;
+
+    public AutorModel create(AutorModel autorModel){
+        return autorRepository.save(autorModel);
+    }
+
+    public List<AutorModel> getAll(){
+        return autorRepository.findAll();
+    }
+
+    public AutorModel getById(Long id){
+        return autorRepository.getById(id);
+    }
+
+    public void deleteById(Long id){
+        autorRepository.deleteById(id);
+    }
+}
